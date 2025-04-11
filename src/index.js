8eeef34f9b65933d8b3eb98a1aa20cd0b57ee83f
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-11 15:07:26 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import { initMongoConnection } from './db/initMongoConnection.js';
+import { connectDB } from './db/connectDB.js';
+import { setupServer } from './server.js';
+
+await connectDB();
+setupServer();
 import { setupServer } from './server.js';
 
 await initMongoConnection();

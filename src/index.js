165ee17312984133f0d81b632320bdbe5756b4fb
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-12 15:53:30 +0300

    S

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import connectDB from './db/connectDB.js';
 import { setupServer } from './server.js';
 
-await connectDB();
-setupServer();
+await connectDB();       // Підключення до бази
+setupServer();           // Запуск сервера
+

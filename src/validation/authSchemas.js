01df558a2a285f24a1e47072dd48bc37d77561bd
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-12 17:04:37 +0300

    second

---
+++ src/validation/authSchemas.js
@@ -0,0 +1,13 @@
+// src/validations/authSchemas.js
+import Joi from 'joi';
+
+export const registerSchema = Joi.object({
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});

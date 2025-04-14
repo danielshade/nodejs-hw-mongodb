d56cc6cc201f1fddbf108d08e421f9626412f33d
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-14 17:11:52 +0300

    Виправлено запуск сервера без setupServer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-// Якщо раніше було: import { setupServer } from './server.js';
-// Просто замініть на:
 import './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-
-const bootstrap = async () => {
-  await initMongoConnection();
-  setupServer();
-};
-
-bootstrap();

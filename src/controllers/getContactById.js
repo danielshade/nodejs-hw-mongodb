4c0c1f438d2238ec2ca2e5dae99d46ac8bfbc723
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-15 14:42:50 +0300

    bg

--- src/controllers/getContactById.js
+++ src/controllers/getContactById.js
@@ -1,20 +1,20 @@
 import getContactByIdService from '../services/getContactById.js';
 
-export const getContactByIdController = async (req, res, next) => {
+const getContactById = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await getContactByIdService(id);
-
     if (!contact) {
-      return res.status(404).json({ message: 'Contact not found' });
+      return res.status(404).json({ message: 'Not found' });
     }
-
     res.status(200).json({
       status: 200,
-      message: `Successfully found contact with id ${id}`,
+      message: `Successfully found contact with id ${id}!`,
       data: contact,
     });
   } catch (error) {
     next(error);
   }
 };
+
+export default getContactById;
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import getAllContacts from '../controllers/getAllContacts.js';
+import getContactById from '../controllers/getContactById.js';
 
 const router = express.Router();
 
-router.get('/', getAllContacts); // /contacts
+router.get('/', getAllContacts);
+router.get('/:id', getContactById); 
 
 export default router;

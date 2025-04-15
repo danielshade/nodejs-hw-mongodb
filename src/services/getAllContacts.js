bb1ada8c9296bda34f54a8ba13a22919577f4ab4
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-15 13:27:33 +0300

    fix: default export in getAllContacts service and controller

--- src/services/getAllContacts.js
+++ src/services/getAllContacts.js
@@ -1,6 +1,12 @@
 import Contact from '../models/contact.js';
 
-export async function getAllContacts(req, res) {
-  const contacts = await Contact.find();
-  return contacts;
-}
+const getAllContactsService = async () => {
+  try {
+    const contacts = await Contact.find();
+    return contacts;
+  } catch (error) {
+    throw new Error('Не вдалося отримати список контактів');
+  }
+};
+
+export default getAllContactsService;

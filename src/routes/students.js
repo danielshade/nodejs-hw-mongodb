1853cff77a7b5dc7c88082e496e7660e3c92a1a2
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-15 18:12:48 +0300

    s

--- src/routes/students.js
+++ src/routes/students.js
@@ -3,6 +3,6 @@ import addStudent from '../controllers/addStudent.js';
 
 const router = express.Router();
 
-router.post('/', addStudent); // POST /students
+router.post('/', addStudent); // ← це має бути
 
 export default router;

20d027675ae450ca526fe6823b5a18584800f0d9
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-16 20:45:17 +0300

    Update initMongoConnection.js

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,16 +1,20 @@
 import mongoose from 'mongoose';
+
 import { getEnvVar } from '../utils/getEnvVar.js';
 
-export async function initMongoConnection() {
+export const initMongoConnection = async () => {
   try {
     const user = getEnvVar('MONGODB_USER');
-    const password = getEnvVar('MONGODB_PASSWORD');
+    const pwd = getEnvVar('MONGODB_PASSWORD');
     const url = getEnvVar('MONGODB_URL');
     const db = getEnvVar('MONGODB_DB');
+
     await mongoose.connect(
-      `mongodb+srv://${user}:${password}@${url}/${db}?retryWrites=true&w=majonity`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=homeWork`,
     );
-  } catch (error) {
-    console.error(error);
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
   }
-}
+};

7fb15ba2bd6943ac949134185829b53168ec8d3d
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-16 15:55:06 +0300

    Update contact.js

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,11 +1,33 @@
-import mongoose from 'mongoose';
+import { model, Schema } from 'mongoose';
 
-const contactSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  phone: String,
-});
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
-const Contact = mongoose.model('Contact', contactSchema);
-
-export default Contact;
+export const contactsCollection = model('contacts', contactsSchema);

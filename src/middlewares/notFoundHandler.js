ff77a5f71e194078d11470177a4bac4735bd0b0c
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-16 21:08:05 +0300

    р

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,14 +1,6 @@
 
-<<<<<<< HEAD
 export const notFoundHandler = (req, res) => {
   res.status(404).json({
     message: 'Route not found',
   });
 };
-=======
- export const notFoundHandler = (req, res) => {
-    res.status(404).json({
-      message: 'Route not found',
-    });
-  };
->>>>>>> 9fd3b270f1ba0c3fe29369c19336d5570004c794

c5d3e931b3bd9f4bcf5b551f5110ab8a3e9393ed
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-16 20:44:40 +0300

    Update notFoundHandler.js

--- src/middlewares/notFoundHandler.js
+++ src/middlewares/notFoundHandler.js
@@ -1,5 +1,6 @@
-export const notFoundHandler = (req, res) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-};
+
+ export const notFoundHandler = (req, res) => {
+    res.status(404).json({
+      message: 'Route not found',
+    });
+  };

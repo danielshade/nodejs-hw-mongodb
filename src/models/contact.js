07f4f31c8c7712feb16b8f0fd4f38f9898205a62
danielshade <113280712+danielshade@users.noreply.github.com>
2025-04-16 15:14:11 +0300

    а

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,33 +1,10 @@
 import mongoose from 'mongoose';
 
-const contactSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavorite: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      required: true,
-      enum: ['work', 'home', 'personal'],
-      default: 'personal',
-    },
-  },
-  { timestamps: true },
-);
+const contactSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  phone: String,
+});
 
 const Contact = mongoose.model('Contact', contactSchema);
 

7d5649119fe56844efbcca9640e40a0af9f3fc42
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-03 15:42:39 +0300

    v

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
 
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';

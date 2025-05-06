82c5fc9ca402bb5092d1970a270d1d0f07eee30c
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-06 13:10:41 +0300

    d

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,9 +4,7 @@ import { getEnvVar } from '../utils/getEnvVar.js';
 export const initMongoConnection = async () => {
   try {
     const uri = getEnvVar('MONGODB_URI');
-
     await mongoose.connect(uri);
-
     console.log('✅ Mongo connection successfully established!');
   } catch (e) {
     console.error('❌ Error while setting up mongo connection:', e);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
-import dotenv from 'dotenv';
-dotenv.config();
 
-import authRouter from './routes/auth.js';
-
-app.use('/auth', authRouter);
-
-
+import { authRouter } from './routers/auth.js'; 
 
 import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
 import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
+
+dotenv.config();
+
+const app = express(); 
+
+app.use('/auth', authRouter);
 
 const startServer = async () => {
   await initMongoConnection();
   await createDirIfNotExists(TEMP_UPLOAD_DIR);
   await createDirIfNotExists(UPLOAD_DIR);
-  setupServer();
+  setupServer(app); 
 };
 
-void startServer();
+startServer();

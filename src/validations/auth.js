adceae4f29768fffeede08b485311d43c9b2b6ee
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-09 17:15:31 +0300

    ф

--- src/validations/auth.js
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-
-export const registerSchema = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});

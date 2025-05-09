d7837d09d6f381b8fc8c571151a18173170645f7
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-09 17:04:04 +0300

    с

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { validateBody } from '../middlewares/validateBody.js';
 import { sendResetEmail, resetPassword } from '../controllers/authController.js';
 import { sendResetEmailSchema, resetPwdSchema } from '../schemas/authSchemas.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
 
 const router = express.Router();
 
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,5 +1,5 @@
  import express from 'express';
- import ctrlWrapper from '../utils/ctrlWrapper.js';
+ import { ctrlWrapper } from '../utils/ctrlWrapper.js';
  import {
    getAllContacts,
    getContactById,

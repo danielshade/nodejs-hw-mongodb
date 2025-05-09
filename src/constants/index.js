bc8d9d7aa89c235a416ac854f0fd7802f1f2616f
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-09 17:33:22 +0300

    ц

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,7 +2,7 @@ import path from 'path';
 export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUDINARY_CLOUD_NAME',
-  API_KEY: 'CLOUDINARY_API_KEY',
-  API_SECRET: 'CLOUDINARY_API_SECRET',
+  CLOUD_NAME: 'CLOUD_NAME',     // замість 'CLOUDINARY_CLOUD_NAME'
+  API_KEY:   'API_KEY',         // замість 'CLOUDINARY_API_KEY'
+  API_SECRET:'API_SECRET',      // замість 'CLOUDINARY_API_SECRET'
 };

70713e2b9f43196e4f256db9e09d52b581f67ac9
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-09 16:54:07 +0300

    й

--- src/server.js
+++ src/server.js
@@ -8,8 +8,7 @@ import { authRouter } from './routers/auth.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
-import authRouter from './routes/auth.js';
-// ...
+
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 

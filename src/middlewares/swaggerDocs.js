6bb8e556abcd825266d5e51bfc2d7ed0e8a1c872
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-13 14:31:40 +0300

    і

--- src/middlewares/swaggerDocs.js
+++ src/middlewares/swaggerDocs.js
@@ -1,16 +1,12 @@
-import createHttpError from 'http-errors';
+import path from 'path';
+import YAML from 'yamljs';
 import swaggerUI from 'swagger-ui-express';
-import fs from 'node:fs';
 
 import { SWAGGER_PATH } from '../constants/index.js';
 
-export const swaggerDocs = () => {
-  try {
-    const swaggerDoc = JSON.parse(fs.readFileSync(SWAGGER_PATH).toString());
-    return [...swaggerUI.serve, swaggerUI.setup(swaggerDoc)];
-  } catch (err) {
-    console.log(err);
-    return (req, res, next) =>
-      next(createHttpError(500, "Can't load swagger docs"));
-  }
-};
+export function swaggerDocs() {
+  
+  const swaggerDoc = YAML.load(path.resolve(process.cwd(), SWAGGER_PATH));
+
+  return [ swaggerUI.serve, swaggerUI.setup(swaggerDoc) ];
+}
--- src/server.js
+++ src/server.js
@@ -5,27 +5,25 @@ import cookieParser from 'cookie-parser';
 
 import { getEnvVar } from './utils/getEnvVar.js';
 import { contactsRouter } from './routers/contacts.js';
-import { errorHandler } from './middlewares/errorHandler.js';
-import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { authRouter } from './routers/auth.js';
-import { UPLOAD_DIR } from './constants/index.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+const PORT = Number(getEnvVar('PORT', 3000));
 
-export const setupServer = () => {
+export function setupServer() {
   const app = express();
+
   app.use(express.json());
   app.use(cors());
   app.use(logger());
   app.use(cookieParser());
-  app.use('/uploads', express.static(UPLOAD_DIR));
 
   app.use('/contacts', contactsRouter);
   app.use('/auth', authRouter);
 
-  app.use('/uploads', express.static(UPLOAD_DIR));
-  app.use('/api-docs', swaggerDocs());
+  app.use('/api-docs', ...swaggerDocs());
 
   app.all('*', notFoundHandler);
   app.use(errorHandler);
@@ -33,4 +31,4 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-};
+}

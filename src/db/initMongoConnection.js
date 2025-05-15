0d2f7337a4886a8e146a128f439112086be0b640
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-15 23:19:23 +0300

    в

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,14 +1,15 @@
+// src/db/initMongoConnection.js
 import mongoose from 'mongoose';
 import { getEnvVar } from '../utils/getEnvVar.js';
 
 export const initMongoConnection = async () => {
   try {
-    const uri = getEnvVar('MONGODB_URI'); // Тягне повний URI з .env
-    console.log('🔍 Connecting to Mongo with URI:', uri); // Для перевірки
+    const uri = getEnvVar('MONGODB_URI');
+    console.log(`🔍 Connecting to Mongo with URI: ${uri}`);
     await mongoose.connect(uri);
     console.log('✅ Mongo connection successfully established!');
   } catch (e) {
-    console.error('❌ Error while setting up mongo connection:', e);
+    console.log('❌ Error while setting up mongo connection:', e);
     throw e;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+// src/index.js
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
 
 const startServer = async () => {
-  await initMongoConnection();  // ← тільки тут
+  await initMongoConnection();              // ← тільки тут викликаємо підключення
+  await createDirIfNotExists(TEMP_UPLOAD_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 

2a328fccaf45621e00172f60eda306c49a0acafd
danielshade <113280712+danielshade@users.noreply.github.com>
2025-05-15 23:07:14 +0300

    чс

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-initMongoConnection();
 import { setupServer } from './server.js';
 
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
-
 const startServer = async () => {
-  await initMongoConnection();
-  await createDirIfNotExists(TEMP_UPLOAD_DIR);
-  await createDirIfNotExists(UPLOAD_DIR);
+  await initMongoConnection();  // ← тільки тут
   setupServer();
 };
 
-void startServer();
+startServer();

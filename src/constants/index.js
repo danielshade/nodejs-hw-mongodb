ad396dabdec91783f8ac4414b01e2826693238da
danielshade <113280712+danielshade@users.noreply.github.com>
2025-06-02 21:10:31 +0300

    gt

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,9 +21,13 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 export const CLOUDINARY = {
-  CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
+  CLOUD_NAME: 'CLOUDINARY_CLOUD_NAME',
+  API_KEY:    'CLOUDINARY_API_KEY',
+  API_SECRET: 'CLOUDINARY_API_SECRET',
 };
+
+export const JoiSchemas = { /* … */ };
+export const HttpCode = { OK: 200, CREATED: 201, /* … */ };
+export const RESPONSE_MESSAGES = { /* … */ };
 
 export const SWAGGER_PATH = path.join(process.cwd(), 'docs', 'swagger.json');

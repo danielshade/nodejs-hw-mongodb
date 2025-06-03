0c8102f4ee4393acedd8c1c483be7d38a98c7730
danielshade <113280712+danielshade@users.noreply.github.com>
2025-06-03 19:05:36 +0300

    ge

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,6 +1,5 @@
-// src/routers/auth.js
 import { Router } from 'express';
-import validateBody from '../middlewares/validateBody.js';
+import { validateBody } from '../middlewares/validateBody.js';
 import {
   loginUserSchema,
   registerUserSchema,
@@ -15,42 +14,34 @@ import {
   requestResetEmailController,
   resetPasswordController,
   userRegisterController,
-} from '../controllers/authController.js';
+} from '../controllers/auth.js';
 
-const authRouter = Router();
+export const authRouter = Router();
 
-// POST /auth/register
 authRouter.post(
   '/register',
   validateBody(registerUserSchema),
-  controllerWrapper(userRegisterController)
+  controllerWrapper(userRegisterController),
 );
 
-// POST /auth/login
 authRouter.post(
   '/login',
   validateBody(loginUserSchema),
-  controllerWrapper(loginUserController)
+  controllerWrapper(loginUserController),
 );
 
-// POST /auth/refresh
 authRouter.post('/refresh', controllerWrapper(refreshTokenController));
 
-// POST /auth/logout
 authRouter.post('/logout', controllerWrapper(logoutUserController));
 
-// POST /auth/send-reset-email
 authRouter.post(
   '/send-reset-email',
   validateBody(requestResetEmailSchema),
-  controllerWrapper(requestResetEmailController)
+  controllerWrapper(requestResetEmailController),
 );
 
-// POST /auth/reset-pwd
 authRouter.post(
   '/reset-pwd',
   validateBody(resetPasswordSchema),
-  controllerWrapper(resetPasswordController)
+  controllerWrapper(resetPasswordController),
 );
-
-export default authRouter;

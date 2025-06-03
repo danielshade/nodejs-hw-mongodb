ef3155d0b22b6f7f0934ac80d0dafdeb9d229f93
danielshade <113280712+danielshade@users.noreply.github.com>
2025-06-03 20:50:15 +0300

    вм

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -14,7 +14,7 @@ import {
   updateContactSchema,
 } from '../validation/contacts.js';
 import { isValidId } from '../middlewares/isValidId.js';
-import { authenticate } from '../middlewares/authenticate.js';
+import authenticate from '../middlewares/authenticate.js';
 import { upload } from '../middlewares/multer.js';
 
 export const contactsRouter = Router();
